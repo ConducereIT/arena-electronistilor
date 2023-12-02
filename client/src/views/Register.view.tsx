8fd0b980c42ed118a498e72d490ebbde06dfbad8
smiromania <tedygamer7@gmail.com>
2023-12-02 20:16:25 +0200

    Created an alpha version of Register page, waiting for feedback

--- client/src/views/Register.view.tsx
+++ client/src/views/Register.view.tsx
@@ -1,5 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
+
+interface ApiResponse {
+  status: number;
+  user?: {
+    token: string;
+  };
+  errorMsg?: string;
+}
 
 export default function Register() {
-  return <div>Register.view</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  function handleLogin() {
+    const storedToken = localStorage.getItem("api-Token");
+
+    if (storedToken) {
+    } else {
+      const apiUrl = "url-catre-endpointul-de-login";
+
+      const requestData = {
+        email: email,
+        password: password,
+      };
+
+      fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestData),
+      })
+        .then((response) => response.json())
+        .then((data: ApiResponse) => {
+          if (data.status === 200) {
+            localStorage.setItem("api-Token", data.user?.token || "");
+          } else {
+            const errorMsg = data.errorMsg || "Error during login.";
+            setError(errorMsg);
+          }
+        })
+        .catch((error) => {
+          console.error("Error during login request:", error);
+          const errorMsg = "Error during login.";
+          setError(errorMsg);
+        });
+    }
+  }
+
+  return (
+    <div>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={handleLogin}>Submit</button>
+      <div id="error-message">{error && <p>{error}</p>}</div>
+    </div>
+  );
 }
